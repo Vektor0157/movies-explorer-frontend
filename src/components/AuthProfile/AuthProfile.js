1edcbf67c8723714fc2f41e7bbd3e0ff43b2d5c2
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2024-01-06 15:21:26 +0300

    тестовый коммит: проверка на сервере

--- src/components/AuthProfile/AuthProfile.js
+++ src/components/AuthProfile/AuthProfile.js
@@ -7,13 +7,18 @@ function AuthProfile({ name, title, isActive, children, ariaLabel, formType, han
 	const submitButtonClassName = formType === 'register' ? 'auth__submit-register' : 'auth__submit-login';
 	const errorButtonClassName = formType === 'register' ? 'auth__error-register' : 'auth__error-login';
 	const buttonClasses = isActive ? submitButtonClassName : `${submitButtonClassName} auth__submit_inactive`;
+	
+	const handleFormSubmit = (e) => {
+		e.preventDefault(); // Предотвращение перезагрузки страницы по умолчанию
+		handleSubmit(e); // Вызов функции handleSubmit из Login
+	};
 
 	return (
 		<div className={`auth auth_type_${name}`}>
 			<div className="auth__container">
 				<Link className='auth__logo' to="/"><img className='auth__logo-img' src={logoAuthForm} alt={title} /></Link>
 				<h1 className="auth__title">{title}</h1>
-				<form className={`auth__form-element auth__form-element_type_${name}`} name={`${name}-form`} onSubmit={handleSubmit} to="/movies">
+				<form className={`auth__form-element auth__form-element_type_${name}`} name={`${name}-form`} onSubmit={handleFormSubmit} to="/movies">
 					{children}
 					{errorMessageAuth && <span className={errorButtonClassName}>{errorMessageAuth}</span>}
 					<button className={`auth__submit ${buttonClasses}`} type="submit" aria-label={ariaLabel}>{ariaLabel}</button>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -24,7 +24,7 @@ function Login({ handleLogin, errorMessageAuth }) {
 	
 	return (
 		<main className='login'>
-			<AuthProfile title="Рады видеть!" name="login" isLoading={true} ariaLabel="Войти" onSubmit={handleSubmit} gray="Ещё не зарегистрированы?" blue="Регистрация" link="/signup" errorMessageAuth={errorMessageAuth} isActive={isFormValid}>
+			<AuthProfile title="Рады видеть!" name="login" isLoading={true} ariaLabel="Войти" handleSubmit={handleSubmit} gray="Ещё не зарегистрированы?" blue="Регистрация" link="/signup" errorMessageAuth={errorMessageAuth} isActive={isFormValid}>
 				<Input id="email" name="email" className="login__input" type="email" label="Email" required value={email} onChange={handleChangeEmail} placeholder="Email" error={email === '' ? 'Введите email' : ''}/>
 				<Input id="password" className="login__input" type="password" label="Пароль" name="password" minLength="8" maxLength="20" required value={password} onChange={handleChangePassword} placeholder="Пароль" error={password === '' ? 'Введите пароль' : ''} autoComplete="current-password"/>
 			</AuthProfile>
