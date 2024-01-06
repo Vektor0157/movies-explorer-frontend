c69818230aaca2846764c75d57ebbfd1205fbc15
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2024-01-06 16:10:19 +0300

    тестовый коммит: проверка на сервере

--- src/components/AuthProfile/AuthProfile.js
+++ src/components/AuthProfile/AuthProfile.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import './AuthProfile.css';
-import logoAuthForm from '../../images/logo.svg'
+import logoAuthForm from '../../images/logo.svg';
 import { Link } from 'react-router-dom';
 
-function AuthProfile({ name, title, isActive, children, ariaLabel, handleSubmit, gray, blue, link, errorMessageAuth }) {
-  const submitButtonClassName = 'auth__submit-login';
-  const errorButtonClassName = 'auth__error-login';
-  const buttonClasses = isActive ? submitButtonClassName : `${submitButtonClassName} auth__submit_inactive`;
+function AuthProfile({
+	name,
+	title,
+	isLoading,
+	ariaLabel,
+	handleSubmit,
+	gray,
+	blue,
+	link,
+	errorMessageAuth,
+	isActive,
+}) {
+	const submitButtonClassName = 'auth__submit-login';
+	const errorButtonClassName = 'auth__error-login';
+	const buttonClasses = isActive ? submitButtonClassName : `${submitButtonClassName} auth__submit_inactive`;
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    handleSubmit(e);
-  };
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		handleSubmit(e);
+	};
 
-  return (
-    <div className={`auth auth_type_${name}`}>
-      <div className="auth__container">
-        <Link className='auth__logo' to="/"><img className='auth__logo-img' src={logoAuthForm} alt={title} /></Link>
-        <h1 className="auth__title">{title}</h1>
-        <form className={`auth__form-element auth__form-element_type_${name}`} name={`${name}-form`} onSubmit={handleFormSubmit}>
-          {children}
-          {errorMessageAuth && <span className={errorButtonClassName}>{errorMessageAuth}</span>}
-          <button className={`auth__submit ${buttonClasses}`} type="submit" aria-label={ariaLabel}>
-            {ariaLabel}
-          </button>
-        </form>
-        <Link className="auth__link" to={link}>
-          <span className="auth__link-gray">{gray}</span>&nbsp;
-          <span className="auth__link-blue">{blue}</span>
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`auth auth_type_${name}`}>
+			<div className="auth__container">
+				<Link className='auth__logo' to="/"><img className='auth__logo-img' src={logoAuthForm} alt={title} /></Link>
+				<h1 className="auth__title">{title}</h1>
+				<form className={`auth__form-element auth__form-element_type_${name}`} name={`${name}-form`} onSubmit={handleFormSubmit}>
+					{isLoading && <div>Loading...</div>}
+					{errorMessageAuth && <span className={errorButtonClassName}>{errorMessageAuth}</span>}
+					<button className={`auth__submit ${buttonClasses}`} type="submit" aria-label={ariaLabel}>{ariaLabel}</button>
+				</form>
+				<Link className="auth__link" to={link}>
+					<span className="auth__link-gray">{gray}</span>&nbsp;
+					<span className="auth__link-blue">{blue}</span>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default AuthProfile;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,24 +6,32 @@ import Input from '../Input/Input';
 function Login({ handleLogin, errorMessageAuth }) {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [isFormValid, setIsFormValid] = useState(false);
 
 	const handleChangeEmail = (e) => {
 		setEmail(e.target.value);
+		checkFormValidity(e.target.value, password);
 	};
 
 	const handleChangePassword = (e) => {
 		setPassword(e.target.value);
+		checkFormValidity(email, e.target.value);
 	};
 
-	const handleSubmit = (email, password) => {
-		handleLogin(email, password);
+	const checkFormValidity = (emailValue, passwordValue) => {
+		setIsFormValid(emailValue !== '' && passwordValue !== '');
 	};
 
-	const isFormValid = email !== '' && password !== '';
-	
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		if (isFormValid) {
+			handleLogin(email, password);
+		}
+	};
+
 	return (
 		<main className='login'>
-			<AuthProfile title="Рады видеть!" name="login" isLoading={true} ariaLabel="Войти" handleSubmit={handleSubmit} gray="Ещё не зарегистрированы?" blue="Регистрация" link="/signup" errorMessageAuth={errorMessageAuth} isActive={isFormValid}>
+			<AuthProfile title="Рады видеть!" name="login" ariaLabel="Войти" handleSubmit={handleSubmit} gray="Ещё не зарегистрированы?" blue="Регистрация" link="/signup" errorMessageAuth={errorMessageAuth} isActive={isFormValid}>
 				<Input id="email" name="email" className="login__input" type="email" label="Email" required value={email} onChange={handleChangeEmail} placeholder="Email" error={email === '' ? 'Введите email' : ''}/>
 				<Input id="password" className="login__input" type="password" label="Пароль" name="password" minLength="8" maxLength="20" required value={password} onChange={handleChangePassword} placeholder="Пароль" error={password === '' ? 'Введите пароль' : ''} autoComplete="current-password"/>
 			</AuthProfile>
